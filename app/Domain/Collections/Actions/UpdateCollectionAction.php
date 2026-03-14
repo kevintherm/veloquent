ca5916baf11891b545ef1273f2990cd0f4d9e61d
Kevin Darmawan <kevindarmawan022@gmail.com>
2026-03-14 23:15:53 +0700

    added lint api rules on create n update

--- app/Domain/Collections/Actions/UpdateCollectionAction.php
+++ app/Domain/Collections/Actions/UpdateCollectionAction.php
@@ -3,6 +3,9 @@
 namespace App\Domain\Collections\Actions;
 
 use App\Domain\Collections\Models\Collection;
+use App\Domain\QueryCompiler\Services\QueryFilter;
+use App\Domain\SchemaManagement\Services\SchemaChangePlan;
+use Illuminate\Support\Arr;
 
 class UpdateCollectionAction
 {
@@ -23,9 +26,27 @@ class UpdateCollectionAction
                 ->all();
         }
 
+        $allFields = SchemaChangePlan::mergeWithSystemFields(array_merge($collection->fields, $data['fields']));
+        $this->validateApiRules($data['api_rules'], Arr::pluck($allFields, 'name'));
+        
         $collection->update($data);
         $collection->refresh();
 
         return $collection;
+    }
+    
+    private function validateApiRules(array $apiRules, array $fields): void
+    {
+        $expectedKeys = ['list', 'create', 'view', 'update', 'delete'];
+        $missingKeys = array_diff($expectedKeys, array_keys($apiRules));
+
+        if (! empty($missingKeys)) {
+            throw new \InvalidArgumentException('Missing API rules for: '.implode(', ', $missingKeys));
+        }
+
+        foreach ($expectedKeys as $rule) {
+            $query = app(Collection::class)->newQuery();
+            QueryFilter::for($query, $fields)->lint($apiRules[$rule]);
+        }
     }
 }
