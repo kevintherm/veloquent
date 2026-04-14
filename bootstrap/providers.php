ea69a61569eb246da3f4045933367dc6936f871b
Kevin <kevindarmawan022@gmail.com>
2026-04-14 23:48:14 +0700

    fix laravel error

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Providers\AppServiceProvider;
 use App\Providers\DocumentationServiceProvider;
 
 return [
-    AppServiceProvider::class,
     DocumentationServiceProvider::class,
 ];
--- routes/docs.php
+++ routes/docs.php
@@ -3,12 +3,10 @@
 use App\Http\Controllers\DocumentationController;
 use Illuminate\Support\Facades\Route;
 
-if (config('velo.docs.enabled')) {
-    $path = config('velo.docs.path', 'docs');
+$path = 'docs';
 
-    Route::prefix($path)->group(function () use ($path) {
-        Route::redirect('/', $path.'/getting-started/introduction', 302)->name('docs.home');
-        Route::get('/search', [DocumentationController::class, 'search'])->name('docs.search');
-        Route::get('/{file?}', [DocumentationController::class, 'show'])->name('docs.show')->where('file', '.*');
-    });
-}
+Route::prefix($path)->group(function () use ($path) {
+    Route::redirect('/', $path.'/getting-started/introduction', 302)->name('docs.home');
+    Route::get('/search', [DocumentationController::class, 'search'])->name('docs.search');
+    Route::get('/{file?}', [DocumentationController::class, 'show'])->name('docs.show')->where('file', '.*');
+});
