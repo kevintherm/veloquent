cbd002f14d308d600d01f64b07725feca4790924
Kevin <kevindrm0@gmail.com>
2026-05-17 10:53:20 +0700

    refactor: update OtpService namespace and remove unused inspire command in console routes

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Domain\Otp\Services\OtpService;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+use Veloquent\Core\Domain\Otp\Services\OtpService;
 
 Schedule::command('realtime:prune-expired-subscriptions')
     ->everyMinute()
